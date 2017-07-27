c56841be42a26f561b300fffcb6bd8b870a32568
pearl <lu92500@yahoo.com.tw>
2017-07-27 15:45:21 +0800

    Update swiftact.js

--- www/swiftact.js
+++ www/swiftact.js
@@ -4,8 +4,8 @@ exports.version = function(arg0, success, error){
 	exec(success, error, "swiftact", "version",[arg0]);
 }
 
-exports.echo = function(arg0, success, error) {
-    exec(success, error, "swiftact", "echo", [arg0]);
+exports.echo = function(success, error) {
+    exec(success, error, "swiftact", "echo", []);
 };
 
 exports.echojs = function(arg0, success, error) {
