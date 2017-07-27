ff1d4c31a1c252e37dba892492fa2faf7392062b
pearl <lu92500@yahoo.com.tw>
2017-07-27 15:46:08 +0800

    Update swiftact.swift

--- src/ios/swiftact.swift
+++ src/ios/swiftact.swift
@@ -1,10 +1,14 @@
 @objc(swiftact) class swiftact : CDVPlugin {
 
     func version(command: CDVInvokedUrlCommand){
-        var pluginResult:CDVPluginResult?
-        let strVersion = Bundle .main .object(forInfoDictionaryKey: "CFBundleShortVersionString")
-	    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,messageAs: strVersion)
-	    self.commandDelegate!.send(pluginResult,callbackId:command.callbackId)
+        let infoDic = Bundle.main.infoDictionary
+        
+        //APP version
+        let appVersion = infoDic?["CFBundleShortVersionString"]
+        print(appVersion)
+        //var pluginResult:CDVPluginResult?
+	    //pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,messageAs: strVersion)
+	    //self.commandDelegate!.send(pluginResult,callbackId:command.callbackId)
     }
 	
     func openimage(){}
