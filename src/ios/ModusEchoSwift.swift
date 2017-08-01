2d726f0dae753f83e97dbd802833f02dfe499c69
pearl <lu92500@yahoo.com.tw>
2017-08-01 13:48:34 +0800

    Update ModusEchoSwift.swift

--- src/ios/ModusEchoSwift.swift
+++ src/ios/ModusEchoSwift.swift
@@ -1,51 +1,14 @@
 @objc(ModusEchoSwift) class ModusEchoSwift : CDVPlugin {
     func echo(command: CDVInvokedUrlCommand) {
         print("echo echo echo echo")
-        /*var pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_ERROR
-        )
-
-        let msg = command.arguments[0] as? String ?? ""
-
-        if msg.characters.count > 0 {
-            // UIAlertController is iOS 8 or newer only. 
-            let toastController: UIAlertController = 
-                UIAlertController(
-                    title: "", 
-                    message: msg, 
-                    preferredStyle: .Alert
-                )
-
-            self.viewController?.presentViewController(
-                toastController, 
-                animated: true, 
-                completion: nil
-            )
-
-            let duration = Double(NSEC_PER_SEC) * 3.0
-            dispatch_after(
-                dispatch_time(
-                    DISPATCH_TIME_NOW, 
-                    Int64(duration)
-                ), 
-                dispatch_get_main_queue(), 
-                { 
-                    toastController.dismissViewControllerAnimated(
-                        true, 
-                        completion: nil
-                    )
-                }
-            )
-
-            pluginResult = CDVPluginResult(
-                status: CDVCommandStatus_OK,
-                messageAsString: msg
-            )
-        }
-
-        self.commandDelegate!.sendPluginResult(
-            pluginResult, 
+       let msg = command.arguments[0] as? String ?? ""
+       let pluginResult = CDVPluginResult(
+           status: CDVCommendStaus_OK,
+           messageAs: msg+",ECHO"
+       )
+        self.commandDelegate!.send(
+            pluginResult,
             callbackId: command.callbackId
-        )*/
+        )
     }
 }
