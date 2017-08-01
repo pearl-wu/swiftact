2a16482458f966ec9494d694f96a3210a47da16f
pearl <lu92500@yahoo.com.tw>
2017-08-01 13:42:13 +0800

    Update ModusEchoSwift.js

--- www/ModusEchoSwift.js
+++ www/ModusEchoSwift.js
@@ -4,10 +4,10 @@ exports.echo = function(arg0, success, error) {
     exec(success, error, "ModusEchoSwift", "echo", [arg0]);
 };
 
-exports.echojs = function(arg0, success, error) {
+/*exports.echojs = function(arg0, success, error) {
 	if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
 		success(arg0);
 	} else {
 		error('Empty message!');
 	}
-};
+};*/
