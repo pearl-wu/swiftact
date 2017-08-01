b5f36d8d8cd183968d90892e0013d60c77ae8b0c
pearl <lu92500@yahoo.com.tw>
2017-08-01 14:28:30 +0800

    Update ModusSwift.swift

--- src/ios/ModusSwift.swift
+++ src/ios/ModusSwift.swift
@@ -1,11 +1,30 @@
-@objc(ModusSwift) class ModusSwift : CDVPlugin {
-    func version(command: CDVInvokedUrlCommand) {
-        print("echo echo echo echo")
-       let msg = command.arguments[0] as? String ?? ""
-       let pluginResult = CDVPluginResult(
-           status: CDVCommendStaus_OK,
-           messageAs: msg+",ECHO"
-       )
+@objc(ModusEchoSwift) class ModusEchoSwift : CDVPlugin {
+    func echo(_ command: CDVInvokedUrlCommand) {
+        let msg = command.arguments[0] as? String ?? ""
+        var data = ""
+        //print("method call OK!")
+        let infoDic = Bundle.main.infoDictionary
+        if(msg == "version"){
+            // 取App的版本號
+            let appVersion = infoDic?["CFBundleShortVersionString"]
+            data = appVersion as! String
+            // print(appVersion as Any)
+            // 取App的build版本
+            //let appBuildVersion = infoDic?["CFBundleVersion"]
+            //print(appBuildVersion as Any)
+            
+        }else if(msg == "APPname"){
+            // 取App的名稱
+            let appName = infoDic?["CFBundleDisplayName"]
+            data = appName as! String
+            //print(appName as Any)
+        }
+
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: data + ",ECHO"
+        )
+            
         self.commandDelegate!.send(
             pluginResult,
             callbackId: command.callbackId
