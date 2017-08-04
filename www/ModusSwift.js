08a6d7653abfc7fe03942dc4fe1e8d6dbe680038
pearl <lu92500@yahoo.com.tw>
2017-08-04 11:08:16 +0800

    Update ModusSwift.js

--- www/ModusSwift.js
+++ www/ModusSwift.js
@@ -3,7 +3,9 @@ var exec = require('cordova/exec');
 exports.version = function(arg0, success, error) {
     exec(success, error, "ModusSwift", "version", [arg0]);
 };
-
+exports.top = function(arg0, success, error){
+    exec(success, error, "ModusSwift", "toptype", [arg0]);
+};
 /*exports.echojs = function(arg0, success, error) {
 	if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
 		success(arg0);
