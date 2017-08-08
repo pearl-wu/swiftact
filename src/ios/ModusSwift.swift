59ce891fc5487f9f6f357f032738dfd449bfeade
pearl <lu92500@yahoo.com.tw>
2017-08-08 16:28:25 +0800

    Update ModusSwift.swift

--- src/ios/ModusSwift.swift
+++ src/ios/ModusSwift.swift
@@ -33,5 +33,23 @@
     
     func imagecotroller(_ command: CDVInvokedUrlCommand){}
     
-    func imageview(_ command: CDVInvokedUrlCommand){}
+    func fileview(_ command: CDVInvokedUrlCommand){
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        do {
+            // Get the directory contents urls (including subfolders urls)
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil, options: [])
+            print(directoryContents)
+            
+            // if you want to filter the directory contents you can do like this:
+            let mp3Files = directoryContents.filter{ $0.pathExtension == "mp3" }
+            print("mp3 urls:",mp3Files)
+            let mp3FileNames = mp3Files.map{ $0.deletingPathExtension().lastPathComponent }
+            print("mp3 list:", mp3FileNames)
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+
 }
