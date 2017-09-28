de5319a0f45c5b1bb4e0d7977bceb9c5f7168214
pearl <lu92500@yahoo.com.tw>
2017-09-28 01:38:43 -0500

    Update ModusSwift.js

--- www/ModusSwift.js
+++ www/ModusSwift.js
@@ -1,19 +1,36 @@
+cordova.define("cordova-plugins-echoswift.ModusSwift", function(require, exports, module) {
 var exec = require('cordova/exec');
-
-exports.version = function(arg0, success, error) {
-    exec(success, error, "ModusSwift", "version", [arg0]);
-};
-exports.top = function(arg0, success, error){
-    exec(success, error, "ModusSwift", "toptype", [arg0]);
-};
-
-exports.fileview = function(arg0, success,error){
-    exec(success,error,"ModusSwift","fileview",[arg0]);
-}
+               
+   exports.version = function(arg0, success, error) {
+           exec(success, error, "ModusSwift", "version", [arg0]);
+   };
+               
+   exports.top = function(arg0, success, error){
+           exec(success, error, "ModusSwift", "toptype", [arg0]);
+   };
+               
+   exports.fileview = function(arg0, success,error){
+           exec(success,error,"ModusSwift","fileview",[arg0]);
+   }
+               
+   exports.downloadfile = function(arg0, success,error){
+           exec(success,error,"ModusSwift","downloadfile",[arg0]);
+   }
+               
+   exports.isfile = function(arg0, success,error){
+           exec(success,error,"ModusSwift","isfile",[arg0]);
+   }
+               
+   exports.openfile = function(arg0,success,error){
+           exec(success,error,"ModusSwift","openfile",[arg0]);
+   }
+               
 /*exports.echojs = function(arg0, success, error) {
 	if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-		success(arg0);
+ success(arg0);
 	} else {
-		error('Empty message!');
+ error('Empty message!');
 	}
-};*/
+ };*/
+               
+});
