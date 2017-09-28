9062703f273a15f81da91d6b21725509e61eaadd
pearl <lu92500@yahoo.com.tw>
2017-09-28 01:40:23 -0500

    Update ModusSwift.swift

--- src/ios/ModusSwift.swift
+++ src/ios/ModusSwift.swift
@@ -29,27 +29,83 @@
         }
     }
     
-    func download(_ command: CDVInvokedUrlCommand){}
+    func downloadfile(_ command: CDVInvokedUrlCommand){
+        let mag = command.arguments[0] as? String ?? ""
+        let documentsUrl:URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first as URL!
+        let destinationFileUrl = documentsUrl.appendingPathComponent(mag)
+        let fileURL = URL(string:mag)
     
-    func imagecotroller(_ command: CDVInvokedUrlCommand){}
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLRequest(url:fileURL!)
+        
+        
+        
+    }
+    
+    func isfile(_ command: CDVInvokedUrlCommand){
+        let mag = command.arguments[0] as? String ?? ""
+        let fileManager = FileManager.default
+        let filePath:String = NSHomeDirectory() + "/Documents/ \(mag)"
+        let exist = fileManager.fileExists(atPath: filePath)
+        var data:Bool;
+        if exist{
+            data = true;
+        }else{
+            data = false;
+        }
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,messageAs: data)
+        self.commandDelegate!.send(pluginResult,callbackId: command.callbackId)
+    }
+    
+    func openfile(_ command: CDVInvokedUrlCommand){
+        let mag = command.arguments[0] as? String ?? ""
+        let fileManager = FileManager.default
+        let filePath:String = NSHomeDirectory() + "/Documents/ \(mag)"
+        let exist = fileManager.fileExists(atPath: filePath)
+    }
     
     func fileview(_ command: CDVInvokedUrlCommand){
-        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        //let arr = command.arguments[0] as? [String] ?? [""]
+        //let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         
+        let homeDir = NSHomeDirectory();
+        print(homeDir);
+        
+        let fmo=FileManager.default
+        let urls=fmo.urls(for: .documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask)
+        print("-----------")
+        print("base dir: \(urls)")
         do {
+            let filelist=try fmo.contentsOfDirectory(at: urls[0], includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            for file in filelist {
+                print("\(file.lastPathComponent)")
+            }
+        } catch {
+            print(error)
+        }
+        
+        //let manager = FileManager.default
+        //let urlsForDocDirectory = manager.urls(for: FileManager.SearchPathDirectory.documentDirectory,in:FileManager.SearchPathDomainMask.userDomainMask)
+        //let docPath:NSURL = urlsForDocDirectory[0] **as** NSURL
+        //let file = docPath.appendingPathComponent("test.txt")
+        //let attributes = try? manager.sttributesOfItemAtPath(file.path!)
+        //print("attributes: \\(attributes!)")
+        
+        
+        /*do {
             // Get the directory contents urls (including subfolders urls)
             let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil, options: [])
-            print(directoryContents)
+            //print(directoryContents)
             
             // if you want to filter the directory contents you can do like this:
             let mp3Files = directoryContents.filter{ $0.pathExtension == "mp3" }
-            print("mp3 urls:",mp3Files)
+            print("urls:",mp3Files)
             let mp3FileNames = mp3Files.map{ $0.deletingPathExtension().lastPathComponent }
-            print("mp3 list:", mp3FileNames)
+            print("list:", mp3FileNames)
             
         } catch let error as NSError {
             print(error.localizedDescription)
-        }
+        }*/
     }
 
 }
